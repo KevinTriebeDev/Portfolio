76845605f23f22c468c29fdd5ba2fffc258937db
KeinTriebeDev <gamingtriebe@gmail.com>
2026-06-16 11:46:33 +0200

    Add courser pointer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroPage } from './shared/components/hero-page/hero-page';
 import { FooterPage } from './shared/components/footer-page/footer-page';
@@ -11,4 +11,12 @@ import { FooterPage } from './shared/components/footer-page/footer-page';
 })
 export class App {
   protected readonly title = signal('Portfolio');
+  protected readonly cursorX = signal(0);
+  protected readonly cursorY = signal(0);
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent): void {
+    this.cursorX.set(event.clientX);
+    this.cursorY.set(event.clientY);
+  }
 }
