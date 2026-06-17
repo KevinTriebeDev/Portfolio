51747866d90c6d24e29b493182e4a6aba6a84295
KeinTriebeDev <gamingtriebe@gmail.com>
2026-06-17 17:07:22 +0200

    add design html and logic : featured-prjects  fix herader seid spans and eddit h1 wihgt

--- src/app/shared/components/featured-projects-page/featured-projects-page.ts
+++ src/app/shared/components/featured-projects-page/featured-projects-page.ts
@@ -6,4 +6,38 @@ import { Component } from '@angular/core';
   templateUrl: './featured-projects-page.html',
   styleUrl: './featured-projects-page.scss',
 })
-export class FeaturedProjectsPage {}
+export class FeaturedProjectsPage {
+  readonly projects = [
+    {
+      title: 'Join',
+      href: '#',
+      technologies: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+      image: '/assets/img/01.png',
+      alt: 'Preview of the Join project',
+    },
+    {
+      title: 'El Pollo Loco',
+      href: '#',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      image: '/assets/img/02.png',
+      alt: 'Preview of the El Pollo Loco project',
+    },
+    {
+      title: 'DA Bubble',
+      href: '#',
+      technologies: ['Angular', 'Firebase', 'TypeScript'],
+      image: '/assets/img/03.png',
+      alt: 'Preview of the DA Bubble project',
+    },
+  ];
+
+  hoveredProjectIndex: number | null = null;
+
+  setHoveredProject(index: number): void {
+    this.hoveredProjectIndex = index;
+  }
+
+  clearHoveredProject(): void {
+    this.hoveredProjectIndex = null;
+  }
+}
